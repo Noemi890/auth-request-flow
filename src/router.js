663b57271cbd4f0090c4622ecc07c25c2972626f
Noemi <noemi.caggiano@gmail.com>
2022-09-05 15:25:45 +0100

    completed

--- src/router.js
+++ src/router.js
@@ -14,11 +14,29 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, profile, password} = req.body
+
+    const checkUsername = username === mockUser.username
+    const checkPassword = password === mockUser.password
+    try {
+    if (!checkPassword || !checkUsername) throw 'username/password not correct'
+    const userEncoded = jwt.sign({ username, password, profile}, 'mysecretkey')
+    res.json(userEncoded)
+    }
+    catch (e) {
+        res.json(e)
+    }
 
 });
 
 router.get('/profile', (req, res) => {
-  
+  const authorization = req.get('authorization')
+  try {
+    const user = jwt.verify(authorization, 'mysecretkey')
+    res.json(user)
+  } catch (e) {
+    res.json( e.message )
+  }
 });
 
 
